d17162b93886df20d793987fb973230f033c7add
Rabins Rana <rabinrana2058@gmail.com>
2022-04-29 14:37:18 +0545

    Working with date function in javascript

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // console.log("GIT Bootcamp");
-var num1=1;
-var num2=4;
-var result = num1+num2;
-console.log("The Result is: ",result);
+// var num1=1;
+// var num2=4;
+// var result = num1+num2;
+// console.log("The Result is: ",result);
+
+
+let date = new Date();
+let minute=date/86400;
+
+console.log(date);
+console.log(minute);
